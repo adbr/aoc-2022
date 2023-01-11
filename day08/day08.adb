ea7530a53e8cf1411bab38c1258a83efc6747d78
Adam Bryt <adam.bryt@gmx.com>
2023-01-11 14:30:18 +0100

    Day08 - begin

---
+++ day08/day08.adb
@@ -0,0 +1,11 @@
+-- adbr [2023-01-11 Wed]
+-- Usage: day08 filename
+
+with Ada.Text_IO;
+with Ada.Command_Line;
+
+procedure Day08 is
+   use Ada.Text_IO;
+   use Ada.Command_Line;
+begin
+end Day08;
